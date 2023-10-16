56b5a90c8420b3ec6d9f07c66b40fa09769c7494
Sasha20055 <nikolalex783@gmail.com>
2023-10-16 18:54:58 +0300

    Updates

---
+++ static/js/698.cdfbb78e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[698],{698:function(n,t,o){o.r(t),o.d(t,{default:function(){return r}});o(791);var c={},e=o(184),r=function(n){return(0,e.jsx)("div",{className:c.block,children:"Contacts"})}}}]);
+//# sourceMappingURL=698.cdfbb78e.chunk.js.map
