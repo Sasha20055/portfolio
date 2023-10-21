624b32501b1be0c782f0658fe887fbf949df3c88
Sasha20055 <nikolalex783@gmail.com>
2023-10-21 10:45:12 +0300

    projects 2.0

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import "./Navbar.sass"
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import { NavLink } from "react-router-dom";
 
 
 const Navigation = (props) => {
@@ -10,11 +11,11 @@ const Navigation = (props) => {
     <div className="navbarBlock">
       <Navbar bg="dark" data-bs-theme="dark" className="navBar">
         <Container>
-          <Navbar.Brand href="/#about" className="navBrand"></Navbar.Brand>
-          <Nav className="m-auto">
-            <Nav.Link href="/#about">О себе</Nav.Link>
-            <Nav.Link href="/#contacts">Контакты</Nav.Link>
-            <Nav.Link href="/#projects">Проекты</Nav.Link>
+          <Navbar.Brand className="navBrand"><NavLink className="navBrandLink" to={"/about"}></NavLink></Navbar.Brand>
+          <Nav className="m-auto d-flex gap-3">
+            <NavLink to={"/about"} className="text-secondary">О себе</NavLink>
+            <NavLink to={"/contacts"} className="text-secondary">Контакты</NavLink>
+            <NavLink to={"/projects"} className="text-secondary">Проекты</NavLink>
           </Nav>
         </Container>
       </Navbar>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,12 +13,12 @@ const Projects = (props) => {
                 Социальная сеть
               </h3>
               <p className="projects__item-description text-light p-3">
-                Это самый крупный проект, который есть в моем портфолио.
-                Мне потребовалось так много времени, чтобы довести проект до такого уровня.
-                Я не смог закончить его из-за работы, но я сделал очень много.
-                Этому проекту я обязан множеством новых знаний.
-                В этом проекте я использовал TypeScript, SCSS, Firebase Authentication и MongoDB.
-                В этом проекте я изучил MongoDB с нуля.
+                Этот проект на данный момент является наиболее крупным из всех моих проектов, который есть в моем портфолио.
+                Начал разрабатывать по youtube плейлисту <a href="https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8"> Путь самурая </a>
+                канала <a href="https://www.youtube.com/@ITKAMASUTRA"> IT-KAMASUTRA </a>, и вскоре продолжил дорабатывать проект сам.Ему я обязан множеством новых знаний.
+                Социальную сеть я множество раз переделывал, изучая новые архитектурные подходы и постепенно двигаясь дальше.
+                В этом проекте я использовал TypeScript, SASS, React, Redux, Axios, и еще порядка 10 библиотек.
+                С нуля изучил множество из выше перечисленного
               </p>
               <ul className="projects__item-tools d-flex gap-3 flex-wrap mb-3 text-light">
                 <li className="projects__item-tool">React</li>
@@ -26,6 +26,8 @@ const Projects = (props) => {
                 <li className="projects__item-tool">TypeScript</li>
                 <li className="projects__item-tool">Html</li>
                 <li className="projects__item-tool">Html</li>
+                <li className="projects__item-tool">Rest-api</li>
+                <li className="projects__item-tool">axios</li>
                 <li className="projects__item-tool">JavaScript</li>
               </ul>
               <div className="projects__item-buttons d-flex gap-3 justify-content-end">
@@ -38,12 +40,10 @@ const Projects = (props) => {
                 Сайт-лендинг по продвижению заказов
               </h3>
               <p className="projects__item-description text-light p-3">
-                Это самый крупный проект, который есть в моем портфолио.
-                Мне потребовалось так много времени, чтобы довести проект до такого уровня.
-                Я не смог закончить его из-за работы, но я сделал очень много.
-                Этому проекту я обязан множеством новых знаний.
-                В этом проекте я использовал TypeScript, SCSS, Firebase Authentication и MongoDB.
-                В этом проекте я изучил MongoDB с нуля.
+                Этот лендинг делал довольно недавно, по сравнению с прошлыми.
+                Сайт зделан с целью того, чтобы на практике закрепить верстку одностраничных сайтов
+                с использованием библиотеки Bootstrap. Также применил Sass, и библиотеку для создания
+                слайдеров <a href="https://kenwheeler.github.io/slick"> Slick-slider </a> с Jquery.
               </p>
               <ul className="projects__item-tools d-flex gap-3 flex-wrap mb-3 text-light">
                 <li className="projects__item-tool">Javascript</li>
@@ -63,12 +63,10 @@ const Projects = (props) => {
                 Таблица адресов с Яндекс картой
               </h3>
               <p className="projects__item-description text-light p-3">
-                Это самый крупный проект, который есть в моем портфолио.
-                Мне потребовалось так много времени, чтобы довести проект до такого уровня.
-                Я не смог закончить его из-за работы, но я сделал очень много.
-                Этому проекту я обязан множеством новых знаний.
-                В этом проекте я использовал TypeScript, SCSS, Firebase Authentication и MongoDB.
-                В этом проекте я изучил MongoDB с нуля.
+                Проект разрабатывал с целью практики создания React-redux приложений
+                с использоваем сторонних API, таких как <a href="https://pbe-react-yandex-maps.vercel.app/en/"> react-yandex-maps </a>.
+                В этом проекте сам зделал примитивную REST-API, БД которой хранится локально в папке проекта в json формате. Использовал
+                Express для создания REST-API.
               </p>
               <ul className="projects__item-tools d-flex gap-3 flex-wrap mb-3 text-light">
                 <li className="projects__item-tool">React</li>
@@ -76,6 +74,8 @@ const Projects = (props) => {
                 <li className="projects__item-tool">JavaScript</li>
                 <li className="projects__item-tool">Html</li>
                 <li className="projects__item-tool">Sass</li>
+                <li className="projects__item-tool">Rest-api</li>
+                <li className="projects__item-tool">Express</li>
               </ul>
               <div className="projects__item-buttons d-flex gap-3 justify-content-end">
                 <a className="projects__item-gitHub text-info" href="https://github.com/Sasha20055/react-address-table">Код проекта</a>
@@ -86,16 +86,18 @@ const Projects = (props) => {
                 Сайт-лендинг онлайн школы
               </h3>
               <p className="projects__item-description text-light p-3">
-                Это самый крупный проект, который есть в моем портфолио.
-                Мне потребовалось так много времени, чтобы довести проект до такого уровня.
-                Я не смог закончить его из-за работы, но я сделал очень много.
-                Этому проекту я обязан множеством новых знаний.
-                В этом проекте я использовал TypeScript, SCSS, Firebase Authentication и MongoDB.
-                В этом проекте я изучил MongoDB с нуля.
+                Этот лендинг зделал с целью изучения верстки на практике без применения Bootstrap
+                библиотеки. Для создания слайдера применил
+                <a href="https://kenwheeler.github.io/slick"> Slick-slider </a>, для эффекта плавного появления
+                элементов при прокрутке страницы библиотеку <a href="https://wowjs.uk"> Wow.js </a>.
+                Зделал checkbox, который меняет цену на курс (Месячный/Годовой).
               </p>
               <ul className="projects__item-tools d-flex gap-3 flex-wrap mb-3 text-light">
                 <li className="projects__item-tool">Html</li>
                 <li className="projects__item-tool">Sass</li>
+                <li className="projects__item-tool">Wow.js</li>
+                <li className="projects__item-tool">Slick-slider</li>
+                <li className="projects__item-tool">Jquery</li>
                 <li className="projects__item-tool">JavaScript</li>
               </ul>
               <div className="projects__item-buttons d-flex gap-3 justify-content-end">
@@ -108,16 +110,17 @@ const Projects = (props) => {
                 Сайт-лендинг школьного класса
               </h3>
               <p className="projects__item-description text-light p-3">
-                Это самый крупный проект, который есть в моем портфолио.
-                Мне потребовалось так много времени, чтобы довести проект до такого уровня.
-                Я не смог закончить его из-за работы, но я сделал очень много.
-                Этому проекту я обязан множеством новых знаний.
-                В этом проекте я использовал TypeScript, SCSS, Firebase Authentication и MongoDB.
-                В этом проекте я изучил MongoDB с нуля.
+                Этот лендинг зделал как годовой проект по Информатике и Обществознанию еще в 11 классе.
+                В нем хранятся проекты по Обществознанию учеников 11 класса. Применил в проекте переход к
+                Гугл-таблице из за неопытности на тот момент и упрощения функционала.
+                Сайт получился довольно простым, но свой функционал он выполнил. Использовал Html, sass, Javascript, Jquery,
+                <a href="https://kenwheeler.github.io/slick"> Slick-slider </a>.
+
               </p>
               <ul className="projects__item-tools d-flex gap-3 flex-wrap mb-3 text-light">
                 <li className="projects__item-tool">Html</li>
                 <li className="projects__item-tool">Sass</li>
+                <li className="projects__item-tool">Slick-slider</li>
                 <li className="projects__item-tool">Javascript</li>
                 <li className="projects__item-tool">Google-table</li>
               </ul>
@@ -131,12 +134,10 @@ const Projects = (props) => {
                 Многофункциональное модальное окно
               </h3>
               <p className="projects__item-description text-light p-3">
-                Это самый крупный проект, который есть в моем портфолио.
-                Мне потребовалось так много времени, чтобы довести проект до такого уровня.
-                Я не смог закончить его из-за работы, но я сделал очень много.
-                Этому проекту я обязан множеством новых знаний.
-                В этом проекте я использовал TypeScript, SCSS, Firebase Authentication и MongoDB.
-                В этом проекте я изучил MongoDB с нуля.
+                За это модальное окно я получил свой первый заработок, но не скажу что его разработка была
+                без проблем. Это модальное окно содержит много логики, которую и надо было прописать в JavaScript.
+                В ходе выполнения я выучил много чего интересного ( Работа с выбранными файлами, анимация, закрепил на практике JavaScript и т.д ).
+                Использовал в проекте Html, Sass и Javascript.
               </p>
               <ul className="projects__item-tools d-flex gap-3 flex-wrap mb-3 text-light">
                 <li className="projects__item-tool">Html</li>
